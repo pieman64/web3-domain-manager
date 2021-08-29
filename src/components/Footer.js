e53c5e2fa1725542a286d8d3d67a2cfe4c045744
PaulUD <60640581+PaulUD@users.noreply.github.com>
2021-08-29 16:44:42 +0300

    auto resize image

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
-//import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Link from '@material-ui/core/Link';
-//import IconButton from '@material-ui/core/IconButton';
-//import GitHubIcon from '@material-ui/icons/GitHub';
 import DHImage from './../images/dh_gallery.png';
 
 const useStyles = makeStyles(() => ({
@@ -18,23 +15,6 @@ const useStyles = makeStyles(() => ({
     marginTop: 20
   }
 }));
-/*
-var win = window,
-  doc = document,
-  docElem = doc.documentElement,
-  body = doc.getElementsByTagName('body')[0],
-  x = win.innerWidth || docElem.clientWidth || body.clientWidth,
-  y = win.innerHeight|| docElem.clientHeight|| body.clientHeight;
-
-//alert(x + ' × ' + y);
-if (x > 720) {
-  alert('Large screen of ' + x + ' pixels wide'); 
-  //<img src={DHImage} alt="DeadHeads"/>
-} else {
-  alert('Small screen of ' + x + ' pixels wide'); 
-  //<img src={DHImage} alt="DeadHeads"/>   
-}
-*/
 
 
 export default function Footer() {
@@ -50,16 +30,8 @@ export default function Footer() {
         The best way to leave feedback for this fork is to star the project or create an issue on <Link href='//github.com/pieman64/web3-domain-manager' target='_blank' rel='noopener'>GitHub</Link>.
       </Typography>
         text: '\n'
-      {/*
-      <Toolbar>
-        <Link href='//github.com/pieman64/web3-domain-manager' target='_blank' rel='noopener'>
-          <IconButton color='default' aria-label='GitHub repo' component='span'>
-            <GitHubIcon fontSize='default' />
-          </IconButton>
-        </Link>
-      </Toolbar>
-      */}
-      <img src={DHImage} alt="DeadHeads"/>;
+        {/*<img src="https://cdn.unstoppabledomains.com/TVCzy2cvXpEFX9bVqFnVdi80Tqk=/images/template/deadhead-gallery-preview.png" alt="DeadHeads"></img>;*/}
+      <img src={DHImage} alt="DeadHeads" width='70%'/>;
       <Typography color='textSecondary' variant='subtitle2' className={classes.info}>
         Click <Link href='//unstoppabledomains.com/deadhead-collection' target='_blank' rel='noopener'>here</Link> to create your own awesome DeadHeads Gallery.
       </Typography>
