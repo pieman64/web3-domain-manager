a8f183949345800b0483a50cc91e0a4ec9c3e436
PaulUD <60640581+PaulUD@users.noreply.github.com>
2021-08-29 14:10:53 +0300

    remove unused components

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
+//import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Link from '@material-ui/core/Link';
-import IconButton from '@material-ui/core/IconButton';
-import GitHubIcon from '@material-ui/icons/GitHub';
+//import IconButton from '@material-ui/core/IconButton';
+//import GitHubIcon from '@material-ui/icons/GitHub';
 import DHImage from './../images/dh_gallery.png';
 
 const useStyles = makeStyles(() => ({
